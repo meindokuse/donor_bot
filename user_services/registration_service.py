365e36223ae1346410ff822d1a0c3f9fc2e49b5b
meindokuse <meindokuse@gmai.com>
2024-10-29 20:57:51 +0300

    Change

--- user_services/registration_service.py
+++ user_services/registration_service.py
@@ -1,87 +1,108 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-from models import DonationCreate
-import aiohttp
-import asyncio
-from api.network_worker import NetWorkWorker
-from models.user import RegRequestCreate
-user_data = {}
-
-
-from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
+from typing import Optional
+from aiogram import Bot, Dispatcher,F,Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, Message
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.utils import executor
+from aiogram.types import InlineKeyboardButton
+from aiohttp import ClientResponse
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from api.network_worker import NetWorkWorker
 
-bot = Bot(token='YOUR_BOT_TOKEN')
-dp = Dispatcher(bot)
-
-# Состояния для регистрации
 class RegistrationStates(StatesGroup):
     username = State()
     email = State()
     password = State()
 
-# Хранилище данных о пользователях
+    cancel = State()
+
 user_data = {}
+reg_states_router = Router()
 
-# Старт регистрации по кнопке callback
-@dp.callback_query_handler(lambda call: call.data == "add_reg_request")
-async def handle_registration_start(call: CallbackQuery):
-    chat_id = call.message.chat.id
-    await call.message.answer("Введите ваше имя пользователя:")
-    await RegistrationStates.username.set()
-    user_data[chat_id] = {}  # Начало сбора данных для пользователя
 
-# Сохранение имени пользователя
-@dp.message_handler(state=RegistrationStates.username)
-async def get_username(message: Message, state: FSMContext):
-    chat_id = message.chat.id
-    username = message.text
-    user_data[chat_id]['username'] = username
+async def reg_add_request_handler(dp:Dispatcher, bot: Bot):
+    @dp.callback_query(lambda call: call.data == "add_reg_request")
+    @reg_states_router(RegistrationStates.name)
+    async def handle_registration_start(call: CallbackQuery):
+        user_data[chat_id] = {}
+        chat_id = call.message.chat.id
+        await call.message.answer("Введите ваше имя пользователя:")
+        await RegistrationStates.username.set()
 
-    await message.answer("Введите ваш email (или нажмите 'Пропустить'):")
-    await RegistrationStates.email.set()  # Переход к следующему состоянию
+    # Сохранение имени пользователя
+    @reg_states_router.message(RegistrationStates.username)
+    async def get_username(message: Message, state: FSMContext):
+        chat_id = message.chat.id
+        username = message.text
+        user_data[chat_id]['username'] = username
 
-# Сохранение email или переход по умолчанию
-@dp.message_handler(state=RegistrationStates.email)
-async def get_email(message: Message, state: FSMContext):
-    chat_id = message.chat.id
-    email = message.text if message.text.lower() != 'пропустить' else None
-    user_data[chat_id]['email'] = email
+        await message.answer("Введите ваш email (или нажмите 'Пропустить'):")
+        await state.set_state(RegistrationStates.email)  # Переход к следующему состоянию
 
-    await message.answer("Введите ваш пароль:")
-    await RegistrationStates.password.set()  # Переход к состоянию пароля
+    # Сохранение email или переход по умолчанию
+    @reg_states_router.message(RegistrationStates.email)
+    async def get_email(message: Message, state: FSMContext):
+        chat_id = message.chat.id
+        email = message.text if message.text.lower() != 'пропустить' else None
+        user_data[chat_id]['email'] = email
 
-# Сохранение пароля и завершение регистрации
-@dp.message_handler(state=RegistrationStates.password)
-async def get_password(message: Message, state: FSMContext):
-    chat_id = message.chat.id
-    password = message.text
-    telegram_id = str(chat_id)
+        await message.answer("Введите ваш пароль:")
+        await state.set_state(RegistrationStates.password)  # Переход к состоянию пароля
 
-    user_data[chat_id]['password'] = password
+    # Сохранение пароля и завершение регистрации
+    @reg_states_router.message(RegistrationStates.password)
+    async def get_password(message: Message, state: FSMContext):
+        chat_id = message.chat.id
+        password = message.text
 
-    # Формируем запрос для регистрации
-    reg_request = {
-        "username": user_data[chat_id]['username'],
-        "telegram_id": telegram_id,
-        "email": user_data[chat_id]['email'],
-        "password": user_data[chat_id]['password']
-    }
+        user_data[chat_id]['password'] = password
 
-    await message.answer(
-        f"Регистрация завершена!\nВаши данные:\n"
-        f"Имя пользователя: {reg_request['username']}\n"
-        f"Email: {reg_request['email']}\n"
-        f"Telegram ID: {reg_request['telegram_id']}"
-    )
+        # Формируем запрос для регистрации
+        
+        builder = InlineKeyboardBuilder()
 
-    # Завершаем состояние и очищаем данные
-    await state.finish()
-    del user_data[chat_id]
+        button1 = InlineKeyboardButton("Готово",callback_data="send_model_to_server")
+        button2 = InlineKeyboardButton("Начать заново",callback_data="add_reg_request")
+
+        builder.add(button1,button2)
+
+        await bot.send_message(
+            f"Ваши данные:\n"
+            f"Имя пользователя: {user_data[chat_id]['username']}\n"
+            f"Email: {user_data[chat_id]['email']}\n"
+            f"Telegram ID: {user_data[chat_id]['telegram_id']}",
+            reply_markup = builder.as_markup()
+        )
+
+    @dp.callback_query(lambda call: call.data == "send_model_to_server")
+    async def send_model_to_server(call: CallbackQuery, state:FSMContext):
+        chat_id = call.message.from_user.id
+        reg_request = {
+            "username": user_data[chat_id]['username'],
+            "telegram_id": chat_id,
+            "email": user_data[chat_id]['email'],
+            "password": user_data[chat_id]['password']
+        }
+        response: Optional[ClientResponse] = NetWorkWorker().send_model('user/add_reg_request',reg_request)
+        if response:
+            response_data = await response.json()
+            if response_data.get('status') == 'success':
+                await bot.send_message(chat_id,"Запрос на регистрацию отправлен, ожидайте одобрения\nМы сообщим вам вердикт")
+        
+        else:
+            await bot.send_message(chat_id,"Похоже что то пошло не так, попробуйте позже")
+
+        del user_data[chat_id]
+        await state.clear()
+    
+    
+
+
+
+
+
+
+        
+
 
 
 
